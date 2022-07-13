e5d354fdb0086c8b2747180e97b5bf09223a70f7
Matt Alexander <dmalexander6@gmail.com>
2022-07-13 17:43:47 -0600

    Complete creating a dynamic Video List component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import exampleVideoData from '../data/exampleVideoData.js';
+import VideoList from './VideoList.js';
 var App = () => (
   <div>
     <nav className="navbar">
@@ -10,7 +12,7 @@ var App = () => (
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <div><VideoList videos={exampleVideoData}/></div>
       </div>
     </div>
   </div>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
+// import Search from './components/Search.js';
+
+
+// import VideoPlayer from './components/VideoPlayer.js';
+// import exampleVideoData from './data/exampleVideoData.js';
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+
